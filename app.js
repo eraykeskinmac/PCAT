2ba4c7483aeef254399aa82dec1dd07a964dd51e
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-05-29 21:46:18 +0300

    model structure created connected with mongodb

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+// connect DB
+mongoose.connect(
+  'mongodb+srv://eraykeskin:eray123@cluster0.2he0g.mongodb.net/pcat-test-db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Route
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos: photos });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 5000;
