4e3782051e10e6f04c170d9cd68702a07de7e972
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-05-31 23:44:44 +0300

    photo upload and information update structure has been created

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./models/Photo');
-
+const fs = require('fs');
 const app = express();
 
 // connect DB
@@ -22,6 +24,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(fileUpload());
 
 // Route
 app.get('/', async (req, res) => {
@@ -40,8 +44,38 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
-  Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  const uploadeImage = req.files.image;
+  const uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.image = req.body.image;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 5000;
